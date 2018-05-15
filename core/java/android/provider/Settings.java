441fe380d5ad94e2d94d55aa4279042bd2a8b385
thecrazyskull <anaskarbila@aospa.co>
2018-05-15 11:58:02 -0400

    policy: Add support for device alert sliders
    * Provide support for alert slider hardware.
    * Ability to swap order of alert slider within system setting.
    * Enable setting config_hasAlertSlider to true and defining
      proc nodes for state path and UEvent match path.
    
    * Contributors:
    thecrazyskull
    carlosavignano
    
    TODO> SystemUI integration.
    
    Forward ported to Pie by @Sirhc95
    
    Change-Id: I46f594923aedf3445ee9cecb16a90c4963f78a89
    Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5650,14 +5650,31 @@ public final class Settings {
         public static final String QS_TRANSPARENCY = "qs_transparency";
 
 
-      /**
+        /**
        * Whether the torch launch gesture to double tap or long press the power button when the
        * screen is off should be enabled. *
        * 0: disabled
        * 1: long tap power for torch
        * @hide
        */
-      public static final String TORCH_POWER_BUTTON_GESTURE = "torch_power_button_gesture";
+         public static final String TORCH_POWER_BUTTON_GESTURE = "torch_power_button_gesture";
+
+        /**
+         * Whether user can swap the order of the Alert Slider.
+         * * Whether user can invert the order of the Alert Slider.
+         * 0: Default
+         * 1: Inverted
+         * @hide
+         */
+        public static final String ALERT_SLIDER_ORDER = "alert_slider_order";
+
+        /**
+         * Preferred silent mode for Alert Slider..
+         * 0: Alarms only.
+         * 1: Total silence
+         * @hide
+         */
+        public static final String ALERT_SLIDER_SILENT_MODE = "alert_slider_silent_mode";
 
         /**
          * Keys we no longer back up under the current schema, but want to continue to
