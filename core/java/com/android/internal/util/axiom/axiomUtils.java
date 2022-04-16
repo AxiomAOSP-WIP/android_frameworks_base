7b6a584776e904913cf8c031c5d35e21354da3ca
cjh1249131356 <cjh1249131356@gmail.com>
2022-04-16 11:41:31 +0800

    base: Always turn on sensors when disabling sleep mode
    * Also verify once to make sure sensors are turned on.
    
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- core/java/com/android/internal/util/axiom/axiomUtils.java
+++ core/java/com/android/internal/util/axiom/axiomUtils.java
@@ -194,7 +194,6 @@ public class axiomUtils {
         private static boolean mLocationState;
         private static boolean mCellularState;
         private static boolean mBluetoothState;
-        private static boolean mSensorState;
         private static int mRingerState;
         private static int mZenState;
 
@@ -408,7 +407,6 @@ public class axiomUtils {
             final boolean disableSensors = Settings.Secure.getIntForUser(mContext.getContentResolver(),
                     Settings.Secure.SLEEP_MODE_SENSORS_TOGGLE, 1, UserHandle.USER_CURRENT) == 1;
             if (disableSensors) {
-                mSensorState = isSensorEnabled();
                 setSensorEnabled(false);
             }
 
@@ -468,8 +466,14 @@ public class axiomUtils {
             // Enable Sensors
             final boolean disableSensors = Settings.Secure.getIntForUser(mContext.getContentResolver(),
                     Settings.Secure.SLEEP_MODE_SENSORS_TOGGLE, 1, UserHandle.USER_CURRENT) == 1;
-            if (disableSensors && mSensorState != isSensorEnabled()) {
-                setSensorEnabled(mSensorState);
+            if (disableSensors) {
+                setSensorEnabled(true);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {}
+                if (!isSensorEnabled()) {
+                    setSensorEnabled(true);
+                }
             }
 
             // Set Ringer mode (0: Off, 1: Vibrate, 2:DND: 3:Silent)
